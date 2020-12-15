38ec90e42087e4e63b8022435676542f13efc336
Pascal Martineau <pascal@lewebsimple.ca>
2020-12-15 16:34:43 -0500

    Add plugins prompt

--- generator/saofile.js
+++ generator/saofile.js
@@ -9,6 +9,26 @@ module.exports = {
       )
     }
   },
+  prompts() {
+    return [
+      {
+        name: 'plugins',
+        message: 'Select the plugins you want',
+        type: 'checkbox',
+        choices: [
+          'lewebsimple/acf-polylang',
+          'lewebsimple/advanced-custom-fields-pro',
+          'lewebsimple/advanced-forms-pro',
+          'lewebsimple/oxygen',
+          'lewebsimple/polylang-pro',
+          'wpackagist-plugin/autodescription',
+          'wpackagist-plugin/loco-translate',
+          'wpackagist-plugin/safe-svg',
+          'wpackagist-plugin/user-switching',
+        ],
+      },
+    ];
+  },
   actions() {
     return [
       {
@@ -19,12 +39,20 @@ module.exports = {
     ];
   },
   async completed() {
+    const { plugins } = this.answers
+
     // Composer install
-    const { error } = await exec('composer install', { cwd: this.outDir });
+    const { error } = await exec(`composer install`, { cwd: this.outDir });
     if(error) {
       console.error(`\nError installing Composer dependencies`,);
       return;
     }
+
+    // Add plugins
+    plugins.forEach(async plugin => {
+      console.log(`\nInstalling ${this.chalk.green(plugin)}`);
+      await exec(`composer require ${plugin}`, { cwd: this.outDir });
+    });
     
     // Success!
     console.log(`\nSuccessfully created ${this.chalk.green(this.outFolder)} at ${this.chalk.green(this.outDir)}`);
